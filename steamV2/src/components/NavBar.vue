ee506566b7767792e070c4a50d30cd3e526e7b4b
Aleffy-Discente <143960299+Aleffy-Discente@users.noreply.github.com>
2025-08-23 09:36:21 -0300

    Agora o negócio ficou bom
    Falta os favoritos e as outras opções da NavBar. E deixar os jogos em uma API

--- steamV2/src/components/NavBar.vue
+++ steamV2/src/components/NavBar.vue
@@ -1,20 +1,27 @@
 <template>
+  
   <nav class="navbar">
     <div class="logo">
-      <i class="bi bi-controller"></i>
+      <img style="margin-left: 40px;" class="logo_imagem" src="/steamV2/public/imgs/fav.png" alt="logo">
       <p class="game-logo">Game<span class="hub-logo selecionado">Hub</span></p>
     </div>
     <div class="navv">
       <div class="opcoes">
-        <a class="filtro plataforma" id="pc" href="#">PC</a>
+        <a class="filtro plataforma" id="pc" href="#" @click="alerta()">PC</a>
         <p>|</p>
-        <a class="filtro plataforma" id="browser" href="#">Browser</a>
+        <a class="filtro plataforma" id="browser" href="#" @click="alerta()">Browser</a>
         <p>|</p>
-        <a class="filtro plataforma selecionado" id="all" href="#">Todos</a>
+        <a class="filtro plataforma selecionado" id="all" href="#" @click="alerta()">Todos</a>
       </div>
       <div class="fav">
-        <a id="favoritos" class="textt" href="#">Favoritos</a>
+        <a id="favoritos" class="textt" href="#" @click="alerta()">Favoritos</a>
+
       </div>
     </div>
   </nav>
 </template>
+<script setup>
+function alerta() {
+  alert('Função em desenvolvimento')
+}
+</script>
