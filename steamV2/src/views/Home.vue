de2a012e4b69acb69aa6801a368a27e07f5c5ff3
Aleffy-Discente <143960299+Aleffy-Discente@users.noreply.github.com>
2025-08-23 14:59:12 -0300

    FUNCIONA IMAGEEEEEEEM

--- steamV2/src/views/Home.vue
+++ steamV2/src/views/Home.vue
@@ -25,23 +25,23 @@ onMounted(() => { document.title = 'GameHub 🎮' })
 const query = ref('')
 
 const games = ref([
-  { id: 1,  title: 'Counter Strike 1.6', link: 'https://play-cs.com/pt/', img: '/imgs/CS1.6.png' },
-  { id: 2,  title: 'King of Fighters', link: 'https://jogosnainternet.com.br/jogo/the-king-of-the-fighter-2002/', img: '/imgs/KOF.png' },
-  { id: 3,  title: 'Minecraft', link: 'https://eaglercraft.com/', img: '/imgs/Minecraft.jpg' },
-  { id: 4,  title: 'Pokemon Showdown', link: 'https://pokemonshowdown.com/', img: '/imgs/Pokemon.avif' },
-  { id: 5,  title: 'Naruto Vs Bleach', link: 'https://jogosnainternet.com.br/jogo/bleach-vs-naruto-30/', img: '/imgs/NarutovsBleach.jpg' },
-  { id: 6,  title: 'Venge.io', link: 'https://venge.io/', img: '/imgs/Venge.jpg' },
-  { id: 7,  title: 'Supremacy 1914', link: 'https://www.supremacy1914.com/?source=homescreen', img: '/imgs/Supremacy.jpg' },
-  { id: 8,  title: 'Super Mario World', link: 'https://jogosnainternet.com.br/jogo/super-mario-world/', img: '/imgs/SuperMario.jfif' },
-  { id: 9,  title: 'Street Fighter II', link: 'https://www.retrogames.cz/play_304-SNES.php', img: '/imgs/lUTA.jfif' },
-  { id: 10, title: 'Subway Surfers', link: 'https://subway-surfers.org/', img: '/imgs/SubwaySurfers.jfif' },
-  { id: 11, title: 'Marvel Vs Capcom', link: 'https://jogosnainternet.com.br/jogo/marvel-vs-capcom/', img: '/imgs/marvelvscapcom.jpg' },
-  { id: 12, title: 'Make it Meme', link: 'https://makeitmeme.com/pt/', img: '/imgs/Makeitmeme.jfif' },
-  { id: 13, title: 'HaxBall', link: 'https://www.haxball.com/', img: '/imgs/haxball.jpg' },
-  { id: 14, title: 'Poxel.io', link: 'https://poxel.io/', img: '/imgs/poxel.io.png' },
-  { id: 15, title: 'Tower Defense', link: 'https://now.gg/apps/inlogic/51193/tower-defense.html', img: '/imgs/tower.png' },
-  { id: 16, title: 'Punch Hero', link: 'https://now.gg/apps/spatial-io/51691/punch-hero.html', img: '/imgs/punch.png' },
-  { id: 17, title: 'Stumble Guys', link: 'https://www.stumbleguys.com/pt-BR/play', img: '/imgs/Stumble.jpg' }
+  { id: 1,  title: 'Counter Strike 1.6', link: 'https://play-cs.com/pt/', img: '/steamV2/imgs/CS1.6.png' },
+  { id: 2,  title: 'King of Fighters', link: 'https://jogosnainternet.com.br/jogo/the-king-of-the-fighter-2002/', img: '/steamV2/steamV2/imgs/KOF.png' },
+  { id: 3,  title: 'Minecraft', link: 'https://eaglercraft.com/', img: '/steamV2/imgs/Minecraft.jpg' },
+  { id: 4,  title: 'Pokemon Showdown', link: 'https://pokemonshowdown.com/', img: '/steamV2/imgs/Pokemon.avif' },
+  { id: 5,  title: 'Naruto Vs Bleach', link: 'https://jogosnainternet.com.br/jogo/bleach-vs-naruto-30/', img: '/steamV2/imgs/NarutovsBleach.jpg' },
+  { id: 6,  title: 'Venge.io', link: 'https://venge.io/', img: '/steamV2/imgs/Venge.jpg' },
+  { id: 7,  title: 'Supremacy 1914', link: 'https://www.supremacy1914.com/?source=homescreen', img: '/steamV2/imgs/Supremacy.jpg' },
+  { id: 8,  title: 'Super Mario World', link: 'https://jogosnainternet.com.br/jogo/super-mario-world/', img: '/steamV2/imgs/SuperMario.jfif' },
+  { id: 9,  title: 'Street Fighter II', link: 'https://www.retrogames.cz/play_304-SNES.php', img: '/steamV2/imgs/lUTA.jfif' },
+  { id: 10, title: 'Subway Surfers', link: 'https://subway-surfers.org/', img: '/steamV2/imgs/SubwaySurfers.jfif' },
+  { id: 11, title: 'Marvel Vs Capcom', link: 'https://jogosnainternet.com.br/jogo/marvel-vs-capcom/', img: '/steamV2/imgs/marvelvscapcom.jpg' },
+  { id: 12, title: 'Make it Meme', link: 'https://makeitmeme.com/pt/', img: '/steamV2/imgs/Makeitmeme.jfif' },
+  { id: 13, title: 'HaxBall', link: 'https://www.haxball.com/', img: '/steamV2/imgs/haxball.jpg' },
+  { id: 14, title: 'Poxel.io', link: 'https://poxel.io/', img: '/steamV2/imgs/poxel.io.png' },
+  { id: 15, title: 'Tower Defense', link: 'https://now.gg/apps/inlogic/51193/tower-defense.html', img: '/steamV2/imgs/tower.png' },
+  { id: 16, title: 'Punch Hero', link: 'https://now.gg/apps/spatial-io/51691/punch-hero.html', img: '/steamV2/imgs/punch.png' },
+  { id: 17, title: 'Stumble Guys', link: 'https://www.stumbleguys.com/pt-BR/play', img: '/steamV2/imgs/Stumble.jpg' }
 ])
 
 const clean = s => s.normalize('NFD').replace(/\p{Diacritic}/gu, '').toLowerCase()
