a543623d355c3c3312ccdb4739d3e13ed87b5b1a
Aleffy-Discente <143960299+Aleffy-Discente@users.noreply.github.com>
2025-10-23 10:26:06 -0300

    Add game

--- steamV2/src/views/Home.vue
+++ steamV2/src/views/Home.vue
@@ -50,7 +50,8 @@ const games = ref([
   { id: 23, title: 'Emupedia', link: 'https://emupedia.net/beta/emuos/', img: '/steamV2/imgs/emu.png' },
   { id: 24, title: 'Elvenar', link: 'https://br0.elvenar.com/web/glps', img: '/steamV2/imgs/elvenar.webp' },
   { id: 25, title: 'Forge of Empires', link: 'https://br-play.forgeofempires.com/', img: '/steamV2/imgs/forge.webp' },
-  { id: 26, title: 'Death Shot', link: 'https://deadshot.io/', img: '/steamV2/imgs/deathShot.webp' }
+  { id: 26, title: 'Death Shot', link: 'https://deadshot.io/', img: '/steamV2/imgs/deathShot.webp' },
+  { id: 26, title: 'Overtide.io', link: 'https://overtide.io/', img: '/steamV2/imgs/overtide.png' }
 ])
 
 const clean = s => s.normalize('NFD').replace(/\p{Diacritic}/gu, '').toLowerCase()
